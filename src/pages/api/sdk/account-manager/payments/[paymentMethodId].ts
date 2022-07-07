30f6a431ea39f9deec6b1d4d0a15dbbec9dc838f
Pedro Sanders <psanders@fonoster.com>
2022-07-07 11:30:42 -0400

    refactor: environment variable + credentials loading

--- src/pages/api/sdk/account-manager/payments/[paymentMethodId].ts
+++ src/pages/api/sdk/account-manager/payments/[paymentMethodId].ts
@@ -2,12 +2,13 @@ import { AccountManager } from '@fonoster/account-manager'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import { requestHandler } from '@/mods/shared/libs/api'
+import { getAPICredentials } from '@/mods/shared/libs/api/credentials'
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const manager = new AccountManager()
+  const manager = new AccountManager(getAPICredentials())
 
   const handlers = {
     delete: async () =>
