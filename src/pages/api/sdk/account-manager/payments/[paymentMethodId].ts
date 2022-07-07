abe3b9ca0bca9446c965de1013babf3661450548
Pedro Sanders <psanders@fonoster.com>
2022-07-06 21:12:36 -0400

    fix: pointing to internal grpc credentials

--- src/pages/api/sdk/account-manager/payments/[paymentMethodId].ts
+++ src/pages/api/sdk/account-manager/payments/[paymentMethodId].ts
@@ -1,17 +1,13 @@
 import { AccountManager } from '@fonoster/account-manager'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { getUserLogged } from '@/mods/auth/lib/getUserLogged'
 import { requestHandler } from '@/mods/shared/libs/api'
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const manager = new AccountManager({
-    ...(await getUserLogged(req)),
-    endpoint: process.env.AM_SERVER_ENDPOINT,
-  })
+  const manager = new AccountManager()
 
   const handlers = {
     delete: async () =>
