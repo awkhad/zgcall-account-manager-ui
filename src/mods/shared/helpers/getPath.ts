f04b5e83964009224a6936db784d72a55fdcdf55
Efra Peralta <efra23peralta@gmail.com>
2022-11-03 11:54:43 -0400

    test: add logs for testing

--- src/mods/shared/helpers/getPath.ts
+++ src/mods/shared/helpers/getPath.ts
@@ -1,8 +1,11 @@
 import { config } from '../constants/config'
 
-export const getPath = (path: string) =>
-  path.startsWith('/')
+export const getPath = (path: string) => {
+  console.log('FONOSTER: ', config.APP_BASE_PATH, config.APP_URL)
+
+  return path.startsWith('/')
     ? `${config.APP_BASE_PATH}${path}`
     : `${config.APP_BASE_PATH}/${path}`
+}
 
 export const getBaseAPI = () => `${config.APP_URL}${config.APP_BASE_PATH}/api`
